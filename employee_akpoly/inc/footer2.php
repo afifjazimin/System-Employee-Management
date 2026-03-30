5ab50b58bf50c019706032a35aa2680e6d9f3e0f
Afif Jazimin <afifjaz17@gmail.com>
2026-03-30 19:22:52 +0800

    Update footer personal project name

--- employee_akpoly/inc/footer2.php
+++ employee_akpoly/inc/footer2.php
@@ -1,1 +1,1 @@
-<span>Copyright &copy; <?php echo date("Y"); ?> Afif Janimin Personal Project. All rights reserved.</span>
+<span>Copyright &copy; <?php echo date("Y"); ?> Afif Jazimin Personal Project. All rights reserved.</span>
